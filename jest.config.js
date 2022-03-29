6b1b0bc2cc0b2d7cbe8c2fa8ef2804a9fc016573
Uriel Mori <moriuriel@gmail.com>
2022-03-28 21:11:46 -0300

    chore: update gitignore

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,14 @@
+const { pathsToModuleNameMapper } = require('ts-jest')
+const { compilerOptions } = require('./tsconfig.json')
 module.exports = {
   clearMocks: false,
   collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/modules/**/services/*.service.ts'],
   coverageDirectory: 'coverage',
   coverageReporters: ['text-summary', 'lcov'],
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/*.spec.ts'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/src/',
+  }),
 }
